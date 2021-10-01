ea5fc646250fd5eec5cc28d1e09a296cf124cb5a
LachlanCourt <c3308061@uon.edu.au>
2021-10-01 23:06:20 +1000

    Rework play function

--- cogs/Music.py
+++ cogs/Music.py
@@ -3,53 +3,46 @@
 
 import discord
 from discord.ext import commands
+from cogs._helpers.video import Video
 
 class Music(commands.Cog):
 
     def __init__(self, client, config):
         self.client = client
         self.config = config
+        self.now_playing = None
+        self.playlist = []
 
-    def _play_song(self, client, state, song):
-        state.now_playing = song
-        state.skip_votes = set()  # clear skip votes
+    def _play_song(self, client, song):
+        self.now_playing = song
         source = discord.PCMVolumeTransformer(
             discord.FFmpegPCMAudio(song.stream_url, volume=1.0)
 
         client.play(source, after=after_playing)
 
     async def play(self, ctx, *, url):
-        """Plays audio hosted at <url> (or performs a search for <url> and plays the first result)."""
-
         client = ctx.guild.voice_client
-        state = self.get_state(ctx.guild)  # get the guild's state
 
         if client and client.channel:
             try:
                 video = Video(url, ctx.author)
             except youtube_dl.DownloadError as e:
-                logging.warn(f"Error downloading video: {e}")
-                await ctx.send(
-                    "There was an error downloading your video, sorry.")
+                await ctx.send("Couldn't find a video using that search term!")
                 return
-            state.playlist.append(video)
-            message = await ctx.send(
-                "Added to queue.", embed=video.get_embed())
-            await self._add_reaction_controls(message)
+            self.playlist.append(video)
+            message = await ctx.send("Added to queue.", embed=video.get_embed())
+            #await self._add_reaction_controls(message)
         else:
             if ctx.author.voice is not None and ctx.author.voice.channel is not None:
                 channel = ctx.author.voice.channel
                 try:
                     video = Video(url, ctx.author)
                 except youtube_dl.DownloadError as e:
-                    await ctx.send(
-                        "There was an error downloading your video, sorry.")
+                    await ctx.send("Error downloading video")
                     return
                 client = await channel.connect()
-                self._play_song(client, state, video)
+                self._play_song(client, video)
                 message = await ctx.send("", embed=video.get_embed())
-                await self._add_reaction_controls(message)
-                logging.info(f"Now playing '{video.title}'")
+                #await self._add_reaction_controls(message)
             else:
-                raise commands.CommandError(
-                    "You need to be in a voice channel to do that.")
+                await ctx.send("You need to be in a voice channel to send this command")
