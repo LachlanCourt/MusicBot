bb5fad8c6d6a7f4fabe44840b4fb8c264f819150
LachlanCourt <c3308061@uon.edu.au>
2021-10-01 22:50:44 +1000

    Add methods

--- cogs/Music.py
+++ cogs/Music.py
@@ -9,3 +9,47 @@ class Music(commands.Cog):
     def __init__(self, client, config):
         self.client = client
         self.config = config
+
+    def _play_song(self, client, state, song):
+        state.now_playing = song
+        state.skip_votes = set()  # clear skip votes
+        source = discord.PCMVolumeTransformer(
+            discord.FFmpegPCMAudio(song.stream_url, volume=1.0)
+
+        client.play(source, after=after_playing)
+
+    async def play(self, ctx, *, url):
+        """Plays audio hosted at <url> (or performs a search for <url> and plays the first result)."""
+
+        client = ctx.guild.voice_client
+        state = self.get_state(ctx.guild)  # get the guild's state
+
+        if client and client.channel:
+            try:
+                video = Video(url, ctx.author)
+            except youtube_dl.DownloadError as e:
+                logging.warn(f"Error downloading video: {e}")
+                await ctx.send(
+                    "There was an error downloading your video, sorry.")
+                return
+            state.playlist.append(video)
+            message = await ctx.send(
+                "Added to queue.", embed=video.get_embed())
+            await self._add_reaction_controls(message)
+        else:
+            if ctx.author.voice is not None and ctx.author.voice.channel is not None:
+                channel = ctx.author.voice.channel
+                try:
+                    video = Video(url, ctx.author)
+                except youtube_dl.DownloadError as e:
+                    await ctx.send(
+                        "There was an error downloading your video, sorry.")
+                    return
+                client = await channel.connect()
+                self._play_song(client, state, video)
+                message = await ctx.send("", embed=video.get_embed())
+                await self._add_reaction_controls(message)
+                logging.info(f"Now playing '{video.title}'")
+            else:
+                raise commands.CommandError(
+                    "You need to be in a voice channel to do that.")
