49455d7ec3ad396559969e48a4da37c67aeca879
LachlanCourt <c3308061@uon.edu.au>
2021-10-02 00:03:05 +1000

    Remove unnecessary features and refactor

--- cogs/Music.py
+++ cogs/Music.py
@@ -3,7 +3,7 @@
 
 import discord
 from discord.ext import commands
-from cogs._helpers.video import Video
+from cogs._helpers.Song import Song
 
 class Music(commands.Cog):
 
@@ -13,36 +13,41 @@ class Music(commands.Cog):
         self.now_playing = None
         self.playlist = []
 
-    def _play_song(self, client, song):
+    def _play_song(self, voiceClient, song):
         self.now_playing = song
-        source = discord.PCMVolumeTransformer(
-            discord.FFmpegPCMAudio(song.stream_url, volume=1.0)
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song.url), volume=1.0)
+        voiceClient.play(source)
 
-        client.play(source, after=after_playing)
+    @commands.command("play")
+    async def play(self, ctx, searchTerm):
+        voiceClient = ctx.guild.voice_client
 
-    async def play(self, ctx, *, url):
-        client = ctx.guild.voice_client
-
-        if client and client.channel:
-            try:
-                video = Video(url, ctx.author)
-            except youtube_dl.DownloadError as e:
-                await ctx.send("Couldn't find a video using that search term!")
-                return
-            self.playlist.append(video)
-            message = await ctx.send("Added to queue.", embed=video.get_embed())
-            #await self._add_reaction_controls(message)
-        else:
-            if ctx.author.voice is not None and ctx.author.voice.channel is not None:
+        # Only users currently in a voice channel can add to the queue
+        if ctx.author.voice is not None and ctx.author.voice.channel is not None:
+            # If the bot is already playing a song, add it to the queue
+            if voiceClient and voiceClient.channel:
+                try:
+                    song = Song(searchTerm, ctx.author)
+                except youtube_dl.DownloadError as e:
+                    await ctx.send("Couldn't find a video using that search term")
+                    return
+                self.playlist.append(song)
+                message = await ctx.send("Added to queue.", embed=song.get_embed())
+                #await self._add_reaction_controls(message)
+            # If the bot is not currently playing a song, start it playing straight away
+            else:
+            
                 channel = ctx.author.voice.channel
                 try:
-                    video = Video(url, ctx.author)
+                    song = Song(searchTerm, ctx.author)
                 except youtube_dl.DownloadError as e:
                     await ctx.send("Error downloading video")
                     return
-                client = await channel.connect()
-                self._play_song(client, video)
-                message = await ctx.send("", embed=video.get_embed())
+                # The bot has joined a new voice channel so we should clear the old playlist
+                self.playlist = []
+                voiceClient = await channel.connect()
+                self._play_song(voiceClient, song)
+                message = await ctx.send("", embed=song.get_embed())
                 #await self._add_reaction_controls(message)
-            else:
-                await ctx.send("You need to be in a voice channel to send this command")
+        else:
+            await ctx.send("You need to be in a voice channel to send this command")
